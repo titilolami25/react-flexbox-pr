6afaf7220cee2829d948f8e07f663af2dbf103f0
titilolami25 <titilolami25@gmail.com>
2024-05-04 18:53:22 +0100

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import "./App.css";
-import Form from "./form/Form";
+import React from 'react';
+import './App.css';
+import Table from './Table'; 
 
 function App() {
   return (
     <div className="App">
-      <Form/>
-      </div>
+      <div className="table-container"> {/*Added container div for horizontal scrolling*/}
+      <Table />
+    </div>
+    </div>
   );
 }
 
---
+++ src/tableassg/Table.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const CustomTable = () => {
+    const data = [
+      { name: 'Bunmi Adedeji', age: 35, state: 'Oyo' },
+      { name: 'Dele Ogun', age: 29, state: 'Lagos' },
+      { name: 'Bayo Daniel', age: 22, state: 'Ondo'},
+      { name: 'John Okafor', age: 31, state: 'Cross river' },
+      { name: 'Bode Thomas', age: 19, state: 'Lagos' },
+      { name: 'Apkan Morinwo', age: 20, state: 'Edo'},
+    ];
+
+    const handleClick = (row) => {
+      console.log('Clicked Row Data:', row);
+    };
+
+    return (
+      <div className="table-container">
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>State</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index} onClick={() => handleClick(row)}>
+                <td>{row.name}</td>
+                <td>{row.age}</td>
+                <td>{row.state}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
+  return (
+    <div className="App">
+      <h2>React Table</h2>
+      <CustomTable />
+    </div>
+  );
+}
+
+export default App;
